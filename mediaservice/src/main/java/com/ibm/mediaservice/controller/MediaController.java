be0012ca6ea0fac53e155958200c25276c1573ae
JasmineRengaraj <52908128+JasmineRengaraj@users.noreply.github.com>
2021-08-02 11:25:29 +0530

    Update MediaController.java
    added CORS origin

--- mediaservice/src/main/java/com/ibm/mediaservice/controller/MediaController.java
+++ mediaservice/src/main/java/com/ibm/mediaservice/controller/MediaController.java
@@ -21,14 +21,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ibm.mediaservice.dto.MediaDTO;
@@ -39,9 +32,10 @@ import com.ibm.mediaservice.models.AuthenticationResponse;
 import com.ibm.mediaservice.service.MediaService;
 import com.ibm.mediaservice.service.UserServiceImpl;
 import com.ibm.mediaservice.util.JwtUtil;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping({"/authenticate"})
+
 public class MediaController {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
